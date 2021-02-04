c03becbf5163412a2bcc34558870d33d370273df
chl2021 <695748815@qq.com>
2021-02-04 11:05:20 +0800

    mysql host

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -21,8 +21,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': 'framework',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
-        'PASSWORD': '0N9LZr3ksu',                        # 你的数据库password
-        'HOST': '192.168.95.68',                   		   # 数据库HOST
+        'PASSWORD': 'root',                        # 你的数据库password
+        # 'HOST': '192.168.95.68',                   		   # 数据库HOST
+        'HOST': '192.168.38.98',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
