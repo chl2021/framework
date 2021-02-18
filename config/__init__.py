1acff48a7d5e9b2b454c626f24233c5b710daee4
chl2021 <695748815@qq.com>
2021-02-18 16:57:37 +0800

    conf

--- config/__init__.py
+++ config/__init__.py
@@ -14,11 +14,11 @@ from blueapps.core.celery import celery_app
 # SaaS运行版本，如非必要请勿修改
 RUN_VER = 'open'
 # SaaS应用ID
-APP_CODE = ''
+APP_CODE = 'framework'
 # SaaS安全密钥，注意请勿泄露该密钥
-SECRET_KEY = ''
+SECRET_KEY = 'b195f635-fc17-4e81-a9fe-f2c041f6db6a'
 # 蓝鲸SaaS平台URL，例如 http://paas.bking.com
-BK_URL = None
+BK_URL = 'http://paas.ultra.com:80'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(
